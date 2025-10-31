311fd13e91fd54822ace64c04ed5b58244d25e1c
Gabriel Hlaing <h_hlaing@mail.fhsu.edu>
2025-10-31 16:19:30 +0630

    Register and user section working

--- EventBoard/events/models.py
+++ EventBoard/events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    available_seats = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.title} — {self.date}"
--- EventBoard/events/views.py
+++ EventBoard/events/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from django.contrib import messages
+
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, "Account created. Please log in.")
-            return redirect('login')
+            user = form.save()
+            login(request, user)
+            return redirect("events:index")   # go to home after sign up
     else:
         form = UserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
+    return render(request, "registration/register.html", {"form": form})
 
 
 def index(request):
