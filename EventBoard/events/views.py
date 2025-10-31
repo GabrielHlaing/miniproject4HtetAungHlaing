cc997e46164a13e970215542784bee6e520f3712
Gabriel Hlaing <h_hlaing@mail.fhsu.edu>
2025-10-31 16:40:55 +0630

    Events added by admin is displayed to public users.

--- EventBoard/events/views.py
+++ EventBoard/events/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-
+from .models import Event
 
 def register(request):
     if request.method == "POST":
@@ -14,6 +14,6 @@ def register(request):
         form = UserCreationForm()
     return render(request, "registration/register.html", {"form": form})
 
-
 def index(request):
-    return render(request, 'events/index.html', {})
+    events = Event.objects.all().order_by('date')
+    return render(request, 'events/index.html', {'events': events})
